d9be4990d5baa3b2c569685ddff768573bb5b8e9
Maxim-Zavidei <socolanmarcel@gmail.com>
2023-01-17 23:38:11 +0100

    Crearea intiala a proiectului

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Marcel_Socolan_Proiect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
